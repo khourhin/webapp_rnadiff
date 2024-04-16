dd89dcd91230ea83ffeba11d1fa5dc0ec08bd98e
Etienne Kornobis <ekornobis@gmail.com>
2024-04-16 12:46:45 +0200

    Can now select annotation column and improving organization

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # webapp_rnadiff
 
 A streamlit application using Sequana to explore differential analysis results (using sequana.rnadiff.RNADiffResults object).
+
+## Pickling an object
+from sequana.rnadiff import RNADiffResults
+import pickle
+
+rd = RNADiffResults("rnadiff_folder", condition="my_condition")
+with open("test.pkl", "wb") as f:
+	pickle.dump(rd, f)
--- app.py
+++ app.py
@@ -19,36 +19,38 @@ def plot_gene_names(row):
     )
 
 
-def plot_volcano(df, gene_list):
-    v = Volcano(df, annot_col="gene_name")
-    sel = df.query("gene_name in @gene_list").copy()
+def plot_volcano(rd, comparison, annot_col="name", gene_list=None):
+    df = pd.concat([rd.annotation, rd.comparisons[comparison].df], axis=1, join="inner")
 
-    sel.loc[:, "y"] = [20, 25, 120]
-    sel.loc[:, "x"] = [2, 3.5, -3]
+    st.subheader("Volcano plot")
+    plt.figure()
+
+    v = Volcano(df, annot_col=annot_col)
 
     fig, axes = plt.subplots(1, 1, figsize=(8, 6))
     v.plot()
-    plt.scatter(sel.log2FoldChange, -np.log10(sel.padj), facecolor="none", edgecolor="black")
-    sel.apply(plot_gene_names, axis=1)
+
+    if gene_list:
+        sel = df.query("@annot_col in @gene_list").copy()
+
+        sel.loc[:, "y"] = [20, 25, 120]
+        sel.loc[:, "x"] = [2, 3.5, -3]
+
+        plt.scatter(sel.log2FoldChange, -np.log10(sel.padj), facecolor="none", edgecolor="black")
+        sel.apply(plot_gene_names, axis=1)
+
     plt.ylabel("-log10(adjusted pvalue)")
     plt.xlabel("Fold Change")
     plt.xlim([-6, 6])
 
-
-def volcano_plot(rd):
-    df = pd.concat([rd.annotation, rd.comparisons["MTFP1_KO_vs_WT"].df], axis=1, join="inner")
-
-    st.subheader("Volcano plot")
-    plt.figure()
-    plot_volcano(df, ["Nppa", "Nppb", "Mtfp1"])
     st.pyplot(plt)
 
 
-def plot_heatmap(rd, comparison_sel, regulation, conditions, log2FC, alpha):
+def plot_heatmap(rd, comparison_sel, regulation, condition, groups, annot_col="name", log2FC=0, alpha=0.05):
     # gene_ids = rd.annotation.query("gene_name in @total_gene_list[@gene_list]").index
 
-    rd.log2_fc = log2FC
-    rd.alpha = alpha
+    # rd.log2_fc = log2FC
+    # rd.alpha = alpha
 
     gene_list = rd.get_gene_lists()[comparison_sel][regulation]
     print(f"Gene list after removing genes not significative N: {len(gene_list)}")
@@ -58,11 +60,14 @@ def plot_heatmap(rd, comparison_sel, regulation, conditions, log2FC, alpha):
     gene_list = set(gene_list).intersection(set(count_indexes))
     print(f"Gene list after removing genes not present in counts matrix N: {len(gene_list)}")
 
-    samples = list(rd.design_df.query("condition in @conditions").index)
+    samples = list(rd.design_df.query(f"{condition} in @groups").index)
+
     data = rd.counts_norm.loc[list(gene_list), samples]
     # data = rd.counts_norm.loc[list(gene_list),:]
 
-    data = pd.concat([data, rd.annotation.gene_name], axis=1, join="inner").set_index("gene_name")
+    annotation = rd.df.loc[:, ("annotation", annot_col)].rename(index={"f(annotation, {annot_col})": annot_col})
+    annotation.name = annot_col
+    data = pd.concat([data, annotation], axis=1, join="inner").set_index(annot_col)
 
     # Removing genes with expression standard deviation == 0 (otherwise z_score cannot be computed)
     print(f"Number of genes with null std removed: {(data.std(axis=1) == 0).sum()}")
@@ -71,19 +76,20 @@ def plot_heatmap(rd, comparison_sel, regulation, conditions, log2FC, alpha):
     sns.set(font_scale=1)
     p = Clustermap(
         data,
-        sample_groups_df=rd.design_df.query("condition in @conditions"),
+        sample_groups_df=rd.design_df.query(f"{condition} in @groups"),
         z_score=0,
         center=0,
-        sample_groups_sel=["condition"],
+        sample_groups_sel=[condition],
     )
     p.params["legend.sample.bbox"] = (10, 2)
     p.plot()
 
 
 def main():
-    st.title("Heatmaps")
+    st.title("Interactive RNADiff")
 
-    rd_file = st.sidebar.file_uploader("Upload RMADiffResults pickled object:")
+    st.sidebar.subheader("Input file")
+    rd_file = st.sidebar.file_uploader("Upload RNADiffResults pickled object:")
 
     if rd_file:
         rd = pickle.load(rd_file)
@@ -94,19 +100,22 @@ def main():
         st.subheader("Results")
         st.write(rd.df)
 
-        volcano_plot(rd)
-
+        st.sidebar.subheader("General parameters:")
         comparison = st.sidebar.selectbox("Pick comparison:", rd.get_gene_lists().keys())
         regulation = st.sidebar.selectbox("Pick regulation:", ["down", "up", "all", "no_subsetting"])
-        conditions = st.sidebar.multiselect("Pick conditions:", rd.design_df["condition"].unique())
-        log2fc = st.sidebar.number_input("Log2FC:", 1)
-        alpha = st.sidebar.number_input("padj threshold:", 0.05)
+        annot_col = st.sidebar.selectbox("Pick annotation column:", rd.df.annotation.columns)
+
+        st.sidebar.subheader("Heatmap options:")
+        condition = st.sidebar.selectbox("Pick condition:", rd.design_df.columns.unique())
+        groups = st.sidebar.multiselect("Pick groups:", rd.design_df[condition].unique())
+        # log2fc = st.sidebar.number_input("Log2FC:", 1)
+        # alpha = st.sidebar.number_input("padj threshold:", 0.05)
+
+        plot_volcano(rd, comparison, annot_col=annot_col)
 
         plt.figure()
-        plot_heatmap(rd, comparison, regulation, conditions, log2fc, alpha)
+        plot_heatmap(rd, comparison, regulation, condition, groups, annot_col=annot_col)  # , log2fc, alpha)
         st.pyplot(plt)
-
-        comparison_list = st.sidebar.multiselect("Pick comparisons:", rd.get_gene_lists().keys())
 
 
 if __name__ == "__main__":
